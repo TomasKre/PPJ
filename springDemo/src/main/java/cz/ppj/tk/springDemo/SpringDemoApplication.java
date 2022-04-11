e3fb3913fccbaf16f985f790a24c15c1a7e95308
Tomas Krechler <tomas.krechler@tul.cz>
2022-04-11 23:59:20 +0200

    Spring init

---
+++ springDemo/src/main/java/cz/ppj/tk/springDemo/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package cz.ppj.tk.springDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
